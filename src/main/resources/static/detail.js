ceb7c14b2274d8eca26e53fdb89aae7182231398
beom <baek940dog@gmail.com>
2022-12-15 13:44:07 +0900

    수정/삭제 기능 디테일 페이지로 옮김

--- src/main/resources/static/detail.js
+++ src/main/resources/static/detail.js
@@ -18,6 +18,58 @@ $(document).ready(function () {
             $('#contents').text(contents);
             $('#createdAt').text(`최초 작성일 : ${createdAt.getFullYear()}년 ${createdAt.getMonth()}월 ${createdAt.getDate()}일`);
             $('#modifiedAt').text(`마지막 수정일 : ${modifiedAt.getFullYear()}년 ${modifiedAt.getMonth()}월 ${modifiedAt.getDate()}일`);
+
+            let temp_html = `<img id="edit" class="icon-start-edit" src="/images/edit.png" alt="" onclick="editPost()">
+                            <img id="delete" class="icon-delete" src="/images/delete.png" alt="" onclick="deleteOne('${id}')">
+                            <img id="submit" class="icon-end-edit" src="/images/done.png" alt="" onclick="submitEdit('${id}')">`
+
+            $('#footer').append(temp_html)
         }
     })
 })
+
+function editPost() {
+    showEdits();
+    let contents = $(`#contents`).text().trim();
+    $(`#text-area`).val(contents);
+}
+
+function showEdits() {
+    $(`#edit-area`).show();
+    $(`#submit`).show();
+    $(`#delete`).show();
+
+    $(`#contents`).hide();
+    $(`#edit`).hide();
+}
+
+// 메모를 수정합니다.
+function submitEdit(id) {
+    let author = $(`#author`).text().trim();
+    let contents = $(`#text-area`).val().trim();
+
+    let data = {'author': author, 'contents': contents};
+
+    $.ajax({
+        type: 'PUT',
+        url: `/api/posts/${id}`,
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: function (response) {
+            alert('게시글 변경에 성공하였습니다.');
+            window.location.reload();
+        }
+    });
+}
+
+function deleteOne(id) {
+    $.ajax({
+        type: 'DELETE',
+        url: `/api/posts/${id}`,
+        success: function (response) {
+            alert('게시글 삭제에 성공하였습니다.');
+        }
+    })
+
+    location.replace(window.location.host + '/api/home');
+}
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -32,15 +32,7 @@ function getPosts() {
                                                     <p id="${id}-title" class="title"><a href="/api/detail?id=${id}">${title}</a></p>
                                                     <footer id="${id}-author" style="color: grey; font-size: 15px; margin-bottom: 10px">${author}</footer>
                                                     <p id="${id}-contents" style="font-size: 20px; margin-bottom: 5px">${contents}</p>
-                                                    <div id="${id}-editarea" class="edit">
-                                                        <textarea id="${id}-textarea" class="te-edit" name="" id="" cols="30" rows="5"></textarea>
-                                                    </div>
                                                     <footer style="color: grey; font-size: 15px">${date}</footer>
-                                                    <div class="footer">
-                                                        <img id="${id}-edit" class="icon-start-edit" src="/images/edit.png" alt="" onclick="editPost('${id}')">
-                                                        <img id="${id}-delete" class="icon-delete" src="/images/delete.png" alt="" onclick="deleteOne('${id}')">
-                                                        <img id="${id}-submit" class="icon-end-edit" src="/images/done.png" alt="" onclick="submitEdit('${id}')">
-                                                    </div>
                                                 </blockquote>
                                             </div>
                                         </div>`
@@ -67,49 +59,4 @@ function writePost() {
             window.location.reload();
         }
     });
-}
-
-function editPost(id) {
-    showEdits(id);
-    let contents = $(`#${id}-contents`).text().trim();
-    $(`#${id}-textarea`).val(contents);
-}
-
-function showEdits(id) {
-    $(`#${id}-editarea`).show();
-    $(`#${id}-submit`).show();
-    $(`#${id}-delete`).show();
-
-    $(`#${id}-contents`).hide();
-    $(`#${id}-edit`).hide();
-}
-
-// 메모를 수정합니다.
-function submitEdit(id) {
-    let author = $(`#${id}-author`).text().trim();
-    let contents = $(`#${id}-textarea`).val().trim();
-
-    let data = {'author': author, 'contents': contents};
-
-    $.ajax({
-        type: 'PUT',
-        url: `/api/posts/${id}`,
-        contentType: "application/json",
-        data: JSON.stringify(data),
-        success: function (response) {
-            alert('게시글 변경에 성공하였습니다.');
-            window.location.reload();
-        }
-    });
-}
-
-function deleteOne(id) {
-    $.ajax({
-        type: 'DELETE',
-        url: `/api/posts/${id}`,
-        success: function (response) {
-            alert('게시글 삭제에 성공하였습니다.');
-            window.location.reload();
-        }
-    })
 }
